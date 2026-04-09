5af7605a37f7bf2fbe856cd43b96e8c6ce5e4256
DaniSantos05 <100522786@alumnos.uc3m.es>
2026-04-09 19:01:27 +0200

    Movida la validación del CIF a EnterpriseProject y utilizada en el constructor para validar el CIF al crear el proyecto

--- src/main/python/uc3m_consulting/enterprise_project.py
+++ src/main/python/uc3m_consulting/enterprise_project.py
@@ -2,6 +2,8 @@
 import hashlib
 import json
 from datetime import datetime, timezone
+import re
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
 
 class EnterpriseProject:
     """Class representing a project"""
@@ -13,7 +15,7 @@ class EnterpriseProject:
                  department: str,
                  starting_date: str,
                  project_budget: float):
-        self.__company_cif = company_cif
+        self.__company_cif = self.validate_cif(company_cif)
         self.__project_description = project_description
         self.__project_achronym = project_acronym
         self.__department = department
@@ -21,6 +23,51 @@ class EnterpriseProject:
         self.__project_budget = project_budget
         justnow = datetime.now(timezone.utc)
         self.__time_stamp = datetime.timestamp(justnow)
+
+    @staticmethod
+    def validate_cif(cif: str):
+        """validates a cif number """
+        if not isinstance(cif, str):
+            raise EnterpriseManagementException("CIF code must be a string")
+        patron_cif = re.compile(r"^[ABCDEFGHJKNPQRSUVW]\d{7}[0-9A-J]$")
+        if not patron_cif.fullmatch(cif):
+            raise EnterpriseManagementException("Invalid CIF format")
+
+        primera_letra = cif[0]
+        digitos = cif[1:8]
+        caracter_control = cif[8]
+
+        suma_pares_doblados = 0
+        suma_impares = 0
+
+        for i in range(len(digitos)):
+            if i % 2 == 0:
+                digito_doblado = int(digitos[i]) * 2
+                if digito_doblado > 9:
+                    suma_pares_doblados = suma_pares_doblados + (digito_doblado // 10) + (digito_doblado % 10)
+                else:
+                    suma_pares_doblados = suma_pares_doblados + digito_doblado
+            else:
+                suma_impares = suma_impares + int(digitos[i])
+
+        suma_total = suma_pares_doblados + suma_impares
+        ultimo_digito = suma_total % 10
+        digito_control = 10 - ultimo_digito
+
+        if digito_control == 10:
+            digito_control = 0
+
+        letras_control = "JABCDEFGHI"
+
+        if primera_letra in ('A', 'B', 'E', 'H'):
+            if str(digito_control) != caracter_control:
+                raise EnterpriseManagementException("Invalid CIF character control number")
+        elif primera_letra in ('P', 'Q', 'S', 'K'):
+            if letras_control[digito_control] != caracter_control:
+                raise EnterpriseManagementException("Invalid CIF character control letter")
+        else:
+            raise EnterpriseManagementException("CIF type not supported")
+        return cif
 
     def __str__(self):
         return "Project:" + json.dumps(self.__dict__)
