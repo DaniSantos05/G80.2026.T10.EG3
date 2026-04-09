dcbd12dbdef1d0cba7afb588146cce920b51e936
DaniSantos05 <100522786@alumnos.uc3m.es>
2026-04-09 18:40:06 +0200

    Refactorizada la carga del JSON de proyectos en register_project usando el método _cargar_proyectos_del_almacen

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -139,23 +139,16 @@ class EnterpriseManager:
                                         starting_date=date,
                                         project_budget=budget)
 
-        try:
-            with open(PROJECTS_STORE_FILE, "r", encoding="utf-8", newline="") as file:
-                t_l = json.load(file)
-        except FileNotFoundError:
-            t_l = []
-        except json.JSONDecodeError as ex:
-            raise EnterpriseManagementException("JSON Decode Error - Wrong JSON Format") from ex
-
-        for t_i in t_l:
-            if t_i == new_project.to_json():
+        lista_proyectos = self._cargar_proyectos_del_almacen()
+        for proyecto_almacenado in lista_proyectos:
+            if proyecto_almacenado == new_project.to_json():
                 raise EnterpriseManagementException("Duplicated project in projects list")
 
-        t_l.append(new_project.to_json())
+        lista_proyectos.append(new_project.to_json())
 
         try:
             with open(PROJECTS_STORE_FILE, "w", encoding="utf-8", newline="") as file:
-                json.dump(t_l, file, indent=2)
+                json.dump(lista_proyectos, file, indent=2)
         except FileNotFoundError as ex:
             raise EnterpriseManagementException("Wrong file  or file path") from ex
         except json.JSONDecodeError as ex:
