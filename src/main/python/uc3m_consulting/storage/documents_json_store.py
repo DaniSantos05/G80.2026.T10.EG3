565b1a58b7adf9544cc82912b41b9d507f69b462
Adrián <100550286@alumnos.uc3m.es>
2026-04-22 17:45:45 +0200

    Aplicamos el patron Singleton a DocumentsJsonStore. Lo más seguro es aplicarlo de forma individual en cada clase hija.

--- src/main/python/uc3m_consulting/storage/documents_json_store.py
+++ src/main/python/uc3m_consulting/storage/documents_json_store.py
@@ -4,7 +4,15 @@ from main.python.uc3m_consulting.enterprise_manager_config import TEST_DOCUMENTS
 
 
 class DocumentsJsonStore(JsonStoreMaster):
-    """Store for documents"""
+    """Singleton store for documents"""
+
+    __instance = None
+
+    def __new__(cls):
+        """Singleton implementation for DocumentsJsonStore"""
+        if cls.__instance is None:
+            cls.__instance = super(DocumentsJsonStore, cls).__new__(cls)
+        return cls.__instance
 
     def __init__(self):
         super().__init__(TEST_DOCUMENTS_STORE_FILE)
