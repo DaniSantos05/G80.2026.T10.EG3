d876fae1243b116672e92753e242168583098bf2
Adrián <100550286@alumnos.uc3m.es>
2026-04-22 17:46:48 +0200

    Aplicamos el patron Singleton a la clase hija NumDocsJsonStore

--- src/main/python/uc3m_consulting/storage/num_docs_json_store.py
+++ src/main/python/uc3m_consulting/storage/num_docs_json_store.py
@@ -4,7 +4,15 @@ from main.python.uc3m_consulting.enterprise_manager_config import TEST_NUMDOCS_S
 
 
 class NumDocsJsonStore(JsonStoreMaster):
-    """Store for num docs reports"""
+    """Singleton store for num docs reports"""
+
+    __instance = None
+
+    def __new__(cls):
+        """Singleton implementation for NumDocsJsonStore"""
+        if cls.__instance is None:
+            cls.__instance = super(NumDocsJsonStore, cls).__new__(cls)
+        return cls.__instance
 
     def __init__(self):
         super().__init__(TEST_NUMDOCS_STORE_FILE)
