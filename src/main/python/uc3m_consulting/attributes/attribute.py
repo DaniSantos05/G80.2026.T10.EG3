8b2d1432a9d4f41984e5a3b4609bd50f496ae52d
Adrián <100550286@alumnos.uc3m.es>
2026-04-22 11:44:29 +0200

    Creamos la clase genérica Attirbutes en una nueva sub-carpeto dentro de uc3m_consulting

---
+++ src/main/python/uc3m_consulting/attributes/attribute.py
@@ -0,0 +1,27 @@
+import re
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
+
+class Attribute:
+    """Attribute class definition"""
+
+    def __init__(self):
+        self._validation_pattern = r""
+        self._error_message = ""
+        self._attr_value = ""
+
+    def _validate(self, attr_value):
+        """Attribute validation definition"""
+        my_regex = re.compile(self._validation_pattern)
+        regex_matches = my_regex.fullmatch(attr_value)
+        if not regex_matches:
+            raise EnterpriseManagementException(self._error_message)
+        return attr_value
+
+    @property
+    def value(self):
+        """Returns attr_value"""
+        return self._attr_value
+
+    @value.setter
+    def value(self, attr_value):
+        self._attr_value = self._validate(attr_value)
