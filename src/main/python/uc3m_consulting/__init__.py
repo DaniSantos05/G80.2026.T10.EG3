08aebdb8ba8108537a34ffc59242d2ffc965f0a5
Adrián <100550286@alumnos.uc3m.es>
2026-04-24 20:58:35 +0200

    Cambiando los imports, poniéndolos correctamente. Sino pyb no funciona.

--- src/main/python/uc3m_consulting/__init__.py
+++ src/main/python/uc3m_consulting/__init__.py
@@ -1,10 +1,10 @@
 """UC3M CONSULTING MODULE WITH ALL THE FEATURES REQUIRED FOR ACCESS CONTROL"""
 
-from main.python.uc3m_consulting.project_document import ProjectDocument
-from main.python.uc3m_consulting.enterprise_manager import EnterpriseManager
-from main.python.uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
-from main.python.uc3m_consulting.enterprise_project import EnterpriseProject
-from main.python.uc3m_consulting.enterprise_manager_config import (JSON_FILES_PATH,
+from uc3m_consulting.project_document import ProjectDocument
+from uc3m_consulting.enterprise_manager import EnterpriseManager
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
+from uc3m_consulting.enterprise_project import EnterpriseProject
+from uc3m_consulting.enterprise_manager_config import (JSON_FILES_PATH,
                                                        JSON_FILES_TRANSACTIONS,
                                                        PROJECTS_STORE_FILE,
                                                        DOCUMENTS_STORE_FILE,
